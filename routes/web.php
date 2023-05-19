fe97876141334eb2af0a00b5903af0b0736bdc10
Giancarlo Culcay <gieg5c@gmail.com>
2023-05-18 22:09:51 -0500

    views

--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,30 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Route:get     Consultar
- * Route:post    Guardar
- * Route:put     Actualizar
- * Route:delete  Eliminar
- */
-
 Route::get('/', function () {
-    return 'ruta home';
-    // return view('welcome');
+    return view('home');
+    // return 'ruta home';
 });
 
 Route::get('/blog', function () {
-    return 'listado de publicaciones';
+    // simulado consultsa a db
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel'],
+    ];
+
+    return view('blog', ['posts' => $posts]);
 });
 
 Route::get('/blog/{slug}', function ($slug) {
-    return $slug;
+    $post = $slug;
+    return view('post', ['post' => $post]);
 });
+
 
 // request : solicitud o peticion
 Route::get('/buscar', function (Request $request) {
+    // simulado consultsa a db
     return $request->all();
 });
+

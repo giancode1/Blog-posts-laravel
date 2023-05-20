89976dad3bc0ad818c909a0df1b6b93df2a8f138
Giancarlo Culcay <gieg5c@gmail.com>
2023-05-20 16:53:30 -0500

    validación, datos old

--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,22 +1,31 @@
 @csrf
 
 <label for="title" class="block text-sm font-bold mb-2">Título</label>
+<span class="text-xs text-red-600">
+    @error('title')
+        {{ $message }}
+    @enderror
+</span>
 <input
     type="text"
     name="title"
-    value="{{ $post->title }}"
-    {{-- value="{{ old('title', $post->title)  }}" --}}
+    value="{{ old('title', $post->title)  }}"
     class="border-gray-200 text-gray-700 w-full mb-4 rounded"
 >
 
 <label for="body" class="block text-sm font-bold mb-2">Contenido</label>
+<span class="text-xs text-red-600">
+    @error('body')
+        {{ $message }}
+    @enderror
+</span>
 <textarea
     name="body"
     rows="5"
     title="body"
     placeholder=""
     class="border-gray-200 text-gray-700 w-full mb-4 rounded"
->{{$post->body}}
+>{{ old('body', $post->body) }}
 </textarea>
 
 <div class="gap-4 flex">
